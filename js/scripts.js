ea41811fb868fad29d898089eb90783b3eda5e54
Eduardo Barbosa <109044287+udebarbos@users.noreply.github.com>
2023-03-26 20:05:08 -0300

    Java

---
+++ js/scripts.js
@@ -0,0 +1,168 @@
+const data = [
+    {
+        min: 0,
+        max: 18.4,
+        classification: "Menor que 18,5",
+        info: "Magreza",
+        obesity: "0",
+    },
+    {
+        min: 18.5,
+        max: 24.9,
+        classification: "Entre 18,5 e 24,9",
+        info: "Normal",
+        obesity: "0",
+    },
+    {
+        min: 25,
+        max: 29.9,
+        classification: "Entre 25,0 e 29,9",
+        info: "Sobrepeso",
+        obesity: "I",
+    },
+    {
+        min: 30,
+        max: 39.9,
+        classification: "Entre 30,0 e 39,9",
+        info: "Obesidade",
+        obesity: "II",
+    },
+    {
+        min: 40,
+        max: 99,
+        classification: "Maior que 40,0",
+        info: "Obesidade grave",
+        obesity: "III",
+    },
+];
+//seleção de elementos
+
+const imcTable = document.querySelector("#imc-table");
+
+const heightInput = document.querySelector("#height");
+const weightInput = document.querySelector("#weight");
+
+const calcBtn = document.querySelector("#calc-btn");
+const clearBtn = document.querySelector("#clear-btn");
+
+const imcNumber = document.querySelector("#imc-number span")
+const imcinfo = document.querySelector("#imc-info span")
+const backBtn = document.querySelector("#back-btn")
+
+const calcContainer = document.querySelector("#calc-container")
+const resultContainer = document.querySelector("#result-container")
+//Funções
+function createTable(data) {
+    data.forEach((item) => {
+        const div = document.createElement("div");
+        div.classList.add("table-data");
+
+        const classification = document.createElement("p");
+        classification.innerText = item.classification;
+
+        const info = document.createElement("p");
+        info.innerText = item.info;
+
+        const obesity = document.createElement("p");
+        obesity.innerText = item.obesity;
+
+        div.appendChild(classification);
+        div.appendChild(info);
+        div.appendChild(obesity);
+
+        imcTable.appendChild(div);
+    });
+}
+
+//função para limpar os forms
+function cleanInputs() {
+    heightInput.value = ""
+    weightInput.value = ""
+    imcNumber.classList= ""
+    imcinfo.classList =""
+
+}
+
+//função para limitar os caractéres do usuário  
+function validDigits(text) {
+    return text.replace(/[^0-9,]/g, "");
+}
+
+//função para calcular imc
+function calcImc(weight, height) {
+    const imc = (weight / (height * height)).toFixed(1);
+
+    return imc;
+}
+function showOrHideResults() {
+    calcContainer.classList.toggle("hide")
+    resultContainer.classList.toggle("hide")
+}
+
+//eventos
+
+//evento para limpar o formulário
+clearBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    cleanInputs();
+});
+
+backBtn.addEventListener("click", () => {
+    cleanInputs();
+    showOrHideResults();
+});
+
+//evento para detectar modificaçoes no input para limitação
+[heightInput, weightInput].forEach((el) => {
+    el.addEventListener("input", (e) => {
+        const updatedValue = validDigits(e.target.value);
+        e.target.value = updatedValue;
+    })
+})
+
+//evento para trocar virgulas por pontos
+calcBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const weight = weightInput.value.replace(",", ".");
+    const height = heightInput.value.replace(",", ".");
+
+    if (!weight || !height) return;
+    const imc = calcImc(weight, height);
+
+    let info
+    data.forEach((item) => {
+        if (imc >= item.min && imc <= item.max) {
+            info = item.info;
+        }
+    });
+    if (!info) return;
+    imcNumber.innerText = imc
+    imcinfo.innerText = info
+    switch (info) {
+        case "Magreza":
+            imcNumber.classList.add("low");
+            imcinfo.classList.add("low");
+            break;
+        case "Normal":
+            imcNumber.classList.add("good");
+            imcinfo.classList.add("good");
+            break;
+        case "Sobrepeso":
+            imcNumber.classList.add("low");
+            imcinfo.classList.add("low");
+            break;
+        case "Obesidade":
+            imcNumber.classList.add("medium");
+            imcinfo.classList.add("medium");
+            break;
+        case "Obesidade grave":
+            imcNumber.classList.add("high");
+            imcinfo.classList.add("high");
+            break;
+    }
+    showOrHideResults();
+});
+
+//inicialização
+createTable(data);
